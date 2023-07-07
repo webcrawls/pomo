be5db79fb23f877c3e01a6a15f5dff0b9f2e43e5
webcrawls <github.com@kaden.sh>
2023-07-07 04:12:05 -0400

    🐛 remove sheet from storage when deleting
    * previously, deleting functioned more as a 'forget', as the underlying sheet data still existed in storage. now, deleting a page does what you'd expect.

--- src/lib/components/sidebar/SidebarPages.svelte
+++ src/lib/components/sidebar/SidebarPages.svelte
@@ -1,23 +1,31 @@
 <script lang="ts">
-    import {page} from "$app/stores";
-    import {nextId} from "$lib/util.js";
     import type {Writable} from "svelte/store";
     import type {AppInfo} from "$lib/api/app";
-    import {fadeQuick} from "$lib/util.js";
+    import {nextId, fadeQuick} from "$lib/util";
+    import {browser} from "$app/environment";
+    import {page} from "$app/stores";
+    import {goto} from "$app/navigation";
 
     export let app: Writable<AppInfo>
 
-    const addPage = () => {
-        $app = {
-            ...$app, pages: [...($app?.pages ?? []), nextId($app.pages)]
-        }
+    const addPage = () => $app = {
+        ...$app, pages: [...($app?.pages ?? []), nextId($app.pages)]
     }
 
     const removePage = (id: string) => {
+        // Remove reference to page ID from app state
         $app = {
             ...$app,
             pages: $app.pages.filter(page => page !== id),
         }
+
+        // Navigate off removal target if necessary
+        if ($page.params.pageId === id) {
+            goto('/', {replaceState: false, invalidateAll: true})
+        }
+
+        // Remove page from storage
+        if (browser) localStorage.removeItem("pomo:sheet:id")
     }
 
 </script>
