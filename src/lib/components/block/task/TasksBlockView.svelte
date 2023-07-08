58dea62498a0dd0f6dab098d2320a4e5abdc6d83
webcrawls <github.com@kaden.sh>
2023-07-08 08:55:50 -0400

    🎨 reduce boilerplate by collapsing Text/TaskView components into one

--- src/lib/components/block/task/TasksBlockView.svelte
+++ src/lib/components/block/task/TasksBlockView.svelte
@@ -1,10 +1,53 @@
 <script lang="ts">
     import type {TasksBlock} from "$lib/api";
-    import TaskItemView from "$lib/components/block/task/TaskItemView.svelte";
+    import {createEventDispatcher} from "svelte";
 
+    const dispatch = createEventDispatcher()
     export let item: TasksBlock
+    $: dispatch("update", item)
+
+    const handleTaskKey = (event: KeyboardEvent) => {
+        if (event.key === "Enter") {
+            event.preventDefault()
+            item = {
+                ...item,
+                tasks: [
+                    ...item.tasks,
+                    {name: "", completed: false}
+                ]
+            }
+        }
+    }
+
+    const handleTaskInput = (index: number, event: InputEvent) => {
+        const value = event.target.innerText
+        item = {
+            ...item,
+            tasks: item.tasks.map((v, i) => index === i ? {...v, name: value} : v)
+        }
+    }
+
+    const handleTaskDelete = (index: number) => {
+        item = {
+            ...item,
+            tasks: item.tasks.filter((v, i) => i !== index)
+        }
+
+        if (item.tasks.length === 0) {
+            dispatch("delete")
+        }
+    }
 </script>
 
-{#each item.tasks as task, i}
-    <TaskItemView bind:task={item.tasks[i]}/>
-{/each}
+<ul class="task-block">
+    {#each item.tasks as task, i}
+        <li class="task-item"
+            class:task-item-completed={task.completed}
+            class:task-item-placeholder={!task?.name}>
+            <button on:click={handleTaskDelete.bind(this, i)}>x</button>
+            <p on:keydown={handleTaskKey}
+               on:input={handleTaskInput.bind(this, i)}
+               contenteditable="true">{task?.name ?? ""}</p>
+        </li>
+    {/each}
+</ul>
--- src/lib/components/block/text/TextBlockView.svelte
+++ src/lib/components/block/text/TextBlockView.svelte
@@ -1,29 +1,51 @@
 <script lang="ts">
-    import type {TextBlock} from "$lib/api";
-    import TextModelView from "$lib/components/block/text/TextModelView.svelte";
+    import type {TextBlock, TextModel} from "$lib/api";
     import {createEventDispatcher} from "svelte";
+    import {ignoredEditorKeys} from "$lib/util";
 
     export let item: TextBlock
 
+    $: dispatch("update", item)
+
     const dispatch = createEventDispatcher()
-    let editing: boolean = true
+    const handleKeyDown = (index: number, model: TextModel, event: KeyboardEvent) => processModelInput(index, model, event.key)
+    const processModelInput = (index: number, model: TextModel, input: string) => {
+        if (input === "Enter") {
+            // handle Enter -> create new text elemnet
+            dispatch("create")
+            return
+        } else if (ignoredEditorKeys.indexOf(input) !== -1) {
+            // ignore certain special keys
+            return
+        } else if (input === "Backspace") {
+            // handle backspace
+            const newModel = {
+                ...model,
+                text: model.text.slice(0, model.text.length - 1)
+            }
+            item = {
+                ...item,
+                content: item.content.map((m, i) => i === index ? newModel : m)
+            }
+        } else {
+            // handle regular input
+            const newModel = {
+                ...model,
+                text: model.text + input
+            }
 
-    // const handleCreate = () => item = {
-    //     ...item,
-    //     content: [...item.content, {element: "p", text: "hello", style: ""}]
-    // }
-
-    const handleUpdate = (index: number, e) => {
-        dispatch("update", {
-            ...item,
-            content: item.content.map((v, i) => i === index ? e.detail : v)
-        })
+            item = {
+                ...item,
+                content: item.content.map((m, i) => i === index ? newModel : m)
+            }
+        }
     }
 </script>
 
 
 {#each item.content as model, i}
-    <TextModelView {model}
-                   editable={editing}
-                   on:update={handleUpdate.bind(this, i)}/>
+    <svelte:element this={model.element}
+                    contenteditable="true"
+                    class="text-block"
+                    on:keydown|preventDefault={handleKeyDown.bind(this, i, model)}>{model.text}</svelte:element>
 {/each}
